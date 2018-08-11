8d831b04efcd08a58761fdc2e5b521edb76ef57d
d-german <facmet107@gmail.com>
2018-08-11 15:39:38 -0500

    Added more comments

--- Ch12Interface/ISmartDevice.cs
+++ Ch12Interface/ISmartDevice.cs
@@ -6,4 +6,11 @@
         void Fax();
         void Scan();
     }
+
+    /*    
+    ISP: The Interface Segregation Principle states that clients should not be forced to implement interfaces they don't use.
+    Instead of one fat interface many small interfaces are preferred. 
+    https://dzone.com/articles/solid-principles-by-example-interface-segregation
+    https://www.oodesign.com/interface-segregation-principle.html
+    */
 }
