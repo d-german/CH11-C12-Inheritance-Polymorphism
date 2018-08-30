e0e3eccdad700a61e1401c0f5da36ae0f8980aeb
dgerman <damon.german@perceptivesoftware.com>
2018-08-30 13:25:07 -0500

    Made more changes

--- CH11-Inheritance/Asset.cs
+++ CH11-Inheritance/Asset.cs
@@ -4,12 +4,16 @@
     {
         private int assetId = 12355;
 
-        protected Asset(int assetId) => this.assetId = assetId;
+        protected Asset(int assetId)
+        {
+            this.assetId = assetId;
+        }
 
         public string Name { get; set; }
 
         public virtual decimal Liability { get; } = 0; // Virtual
-        public abstract decimal NetValue { get; } // Note empty implementation
+
+        public abstract decimal GetNetValue();
 
         public override string ToString() => $"{Name} id: {assetId}";
     }
@@ -24,8 +28,13 @@
         public decimal Mortgage { get; set; } = 250000;
         public decimal EstimatedHomeValue { get; set; } = 0;
 
-        public override decimal Liability => Mortgage; // Overridden
-        public override decimal NetValue => EstimatedHomeValue - Liability;
+        public override decimal Liability
+        {
+            get { return Mortgage; }
+        }
+
+        public override decimal GetNetValue()
+        { return EstimatedHomeValue - Liability; }
 
         public override string ToString() => $"{base.ToString()} is House Asset";
     }
@@ -40,7 +49,8 @@
         public decimal CurrentPrice { get; set; }
 
         // We won't override Liability here, because the default implementation will do.
-        public override decimal NetValue => CurrentPrice * SharesOwned;
+        public override decimal GetNetValue()
+        { return CurrentPrice * SharesOwned; }
 
         public override string ToString() => $"{base.ToString()} is Stock Asset";
     }
